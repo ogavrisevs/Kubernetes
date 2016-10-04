44877aa6963165ac553377707c7e83deee7dd6cf
kenny13 <kenny13@kenny13s-MacBook-Pro.local>
2016-10-05 00:33:12 +0300

    Added certs for sec. networking

---
+++ k8s-docs/certs.sh
@@ -0,0 +1,9 @@
+
+Generate certs
+---------------
+
+  git clone https://github.com/kubernetes/contrib.git
+  cd /root/contrib/ansible/roles/kubernetes/files/
+  make-ca-cert.sh
+
+https://raw.githubusercontent.com/kubernetes/kubernetes/master/cluster/saltbase/salt/generate-cert/make-ca-cert.sh
