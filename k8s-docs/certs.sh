d24ceca9c52a1ad5ff07f41eab3a5362a86698ba
kenny13 <kenny13@kenny13s-MacBook-Pro.local>
2016-10-07 16:38:33 +0300

    Added authentication and authorization

--- k8s-docs/certs.sh
+++
@@ -1,9 +0,0 @@
-
-Generate certs
----------------
-
-  git clone https://github.com/kubernetes/contrib.git
-  cd /root/contrib/ansible/roles/kubernetes/files/
-  make-ca-cert.sh
-
-https://raw.githubusercontent.com/kubernetes/kubernetes/master/cluster/saltbase/salt/generate-cert/make-ca-cert.sh
